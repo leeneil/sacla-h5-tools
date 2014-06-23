a32211f60056f3a73b66b87bfdef3a6a48959cb4
Po-Nan Li <lishantreasure@gmail.com>
2014-06-23 15:10:35 +0800

    fix critical bug

--- h5data.m
+++ h5data.m
@@ -1,4 +1,4 @@
-function [data] = h5data(filename, varargin)
+function [DATA] = h5data(filename, varargin)
 
 % read h5 info
 info = h5info(filename);
@@ -9,22 +9,33 @@ tags = 1:( length(info.Groups(2).Groups(1).Groups) - 1);
 if ~isempty(varargin)
     detectors = varargin{1};
     if length(varargin) > 1
-        tags = varargin{1};
+        tags = varargin{2};
     end
 end
 
-
 % pre-allocate memory space
-    data = zeros( info.Groups(2).Groups(1).Groups(2).Datasets(1).Dataspace.Size(1),...
-    info.Groups(2).Groups(1).Groups(2).Datasets(1).Dataspace.Size(2),...
-    ( length(info.Groups(2).Groups(1).Groups)-1 ),...
-    length(detectors) );
+d = detectors(1);
+data = zeros( info.Groups(2).Groups(d).Groups(2).Datasets(1).Dataspace.Size(1),...
+info.Groups(2).Groups(d).Groups(2).Datasets(1).Dataspace.Size(2),...
+length(tags) );
+if length(detectors) > 1
+    DATA = { length(detectors) };
+end
 
 for d = 1:length(detectors)
-    disp(['========== detector ' int2str(d) ' ==========']);
-    for t = length(tags)
-        disp(['loading tag #' int2str(t)]);
-        data(:,:,tags(t)) = h5read(filename, [info.Groups(2).Groups(d).Groups(t+1).Name ...
-            '/' info.Groups(2).Groups(d).Groups(t+1).Datasets(1).Name]);
+    for t = 1:length(tags)
+        disp(['loading detector #' int2str(detectors(d)) '/tag #' int2str(tags(t))]);
+        data(:,:,t) = h5read(filename, [info.Groups(2).Groups( detectors(d) ).Groups( tags(t)+1 ).Name ...
+            '/' info.Groups(2).Groups( detectors(d) ).Groups( tags(t)+1 ).Datasets(1).Name]);
+    end
+    if length(detectors) > 1
+        DATA{d} = data;
+        if d == 1
+            data = zeros( info.Groups(2).Groups(detectors(d)+1).Groups(2).Datasets(1).Dataspace.Size(1),...
+            info.Groups(2).Groups(detectors(d)+1).Groups(2).Datasets(1).Dataspace.Size(2),...
+            ( length(info.Groups(2).Groups(detectors(d)+1).Groups)-1 ) );
+        end
+    else
+        DATA = data;
     end
 end
