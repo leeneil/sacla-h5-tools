6bfdd412d5865955494f111b7f8ca3dce64a660f
Po-Nan Li <lishantreasure@gmail.com>
2014-06-23 12:24:07 +0800

    created h5data.m

---
+++ h5data.m
@@ -0,0 +1,28 @@
+function h5data(filename, varargin)
+
+% read h5 info
+info = h5info(filename);
+
+% handle additional arguments
+detectors = 1:2;
+tags = 2:length(info.Groups(2).Groups(1).Groups);
+if ~isempty(varargin)
+    detectors = varargin{1};
+    if length(varargin) > 1
+        tags = varargin{1};
+    end
+end
+
+for d = detectors
+    disp(['========== detector ' int2str(d) ' ==========']);
+    % pre-allocate memory space
+    data = zeros( info.Groups(2).Groups(d).Groups(2).Datasets(1).Dataspace.Size(1),...
+    info.Groups(2).Groups(d).Groups(2).Datasets(1).Dataspace.Size(2),...
+    ( length(info.Groups(2).Groups(d).Groups)-1 ) );
+    for t = tags
+        disp(['loading tag #' int2str(t)]);
+        data(:,:,t-1) = h5read(filename, [info.Groups(2).Groups(d).Groups(t).Name ...
+            '/' info.Groups(2).Groups(d).Groups(t).Datasets(1).Name]);
+    end
+    save( [filename(1:(end-3)) '_d' int2str(d)], 'data');
+end
